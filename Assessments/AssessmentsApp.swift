0ba528f4b308adaef7bf54929a951a354c2facba
Brianna Tran <briannatran45@gmail.com>
2023-10-30 15:41:13 -0700

    Initial Commit

---
+++ Assessments/AssessmentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssessmentsApp.swift
+//  Assessments
+//
+//  Created by bri on 10/30/23.
+//
+
+import SwiftUI
+
+@main
+struct AssessmentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
