6b4007f6051eee411ae38ef1321fbf874828d943
Brianna Tran <briannatran45@gmail.com>
2023-10-31 09:39:06 -0700

    trying to load assessments

---
+++ Assessments/Views/AssessmentRow.swift
@@ -0,0 +1,27 @@
+//
+//  AssessmentRow.swift
+//  Assessments
+//
+//  Created by bri on 10/30/23.
+//
+
+import SwiftUI
+
+struct AssessmentRow: View {
+    var assessment: AssessmentSession
+    
+    var body: some View {
+        HStack{
+            Image(systemName: "doc.plaintext")
+            Text(assessment.title)
+            
+            Spacer()
+        }
+    }
+}
+
+struct AssessmentRow_Previews: PreviewProvider {
+    static var previews: some View {
+        AssessmentRow(assessment: assessmentSessions.results[0])
+    }
+}
